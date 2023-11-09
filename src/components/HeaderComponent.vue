df14523b8026567f354b48802df25bb4fb7af066
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-11-09 15:38:09 +0100

    header-html-css-set

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,7 +1,25 @@
 <template>
-    <nav>
-
-    </nav>
+    <header>
+        <nav class="container d-flex align-items-center justify-content-between">
+            <a class="navbar-brand" href="#">
+                <img src="../assets/img/dc-logo.png" width="30" height="30" alt="">
+            </a>
+            <ul class="nav justify-content-end">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Active</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+              </li>
+            </ul>
+        </nav>
+    </header>
 </template>
 
 <script>
