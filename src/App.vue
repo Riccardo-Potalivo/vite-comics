b7dda8efd3ad0afdc7f8b4d81adb04befcc42958
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-11-09 15:59:39 +0100

    main-component-html-css-done

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <HeaderComponent/>
+  <MainComponent/>
 </template>
 
 <script>
   import HeaderComponent from './components/HeaderComponent.vue';
+  import MainComponent from './components/MainComponent.vue';
   export default {
     components: {
       HeaderComponent,
+      MainComponent,
     },
     data(){
       return {
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <main>
+        <section class="container">
+            <h3>-- Content goes here --</h3>
+        </section>
+    </main>
+</template>
+
+<script>
+  export default {
+    name: 'MainComponent',
+    data(){
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+    @use '../assets/style/partials/variables' as *;
+    
+    main
+    {
+        background-color: $bg-main;
+        color: $white;
+        padding: 40px 0;
+    }
+
+</style>
