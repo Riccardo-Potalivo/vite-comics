14ddc92a6050cefc511763e6cbf1b700708826d6
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-11-09 17:38:15 +0100

    footertop-done

--- src/components/footer/FooterTop.vue
+++ src/components/footer/FooterTop.vue
@@ -1,7 +1,26 @@
 <template>
     <section>
-        <div class="container">
-            top
+        <div class="container d-flex justify-content-around">
+            <div class="d-flex align-items-center gap-3">
+                <img src="../../assets/img/buy-comics-digital-comics.png" alt="">
+                <a href="#">DIGITAL COMICS</a>
+            </div>
+            <div class="d-flex align-items-center gap-3">
+                <img src="../../assets/img/buy-comics-digital-comics.png" alt="">
+                <a href="#">DIGITAL COMICS</a>
+            </div>
+            <div class="d-flex align-items-center gap-3">
+                <img src="../../assets/img/buy-comics-digital-comics.png" alt="">
+                <a href="#">DIGITAL COMICS</a>
+            </div>
+            <div class="d-flex align-items-center gap-3">
+                <img src="../../assets/img/buy-comics-digital-comics.png" alt="">
+                <a href="#">DIGITAL COMICS</a>
+            </div>
+            <div class="d-flex align-items-center gap-3">
+                <img src="../../assets/img/buy-comics-digital-comics.png" alt="">
+                <a href="#">DIGITAL COMICS</a>
+            </div>
         </div>
     </section>
 </template>
@@ -28,6 +47,24 @@
 
 <style lang="scss" scoped>
     @use '../../assets/style/partials/variables' as *;
+
+    section
+    {
+        background-color: $primary;
+        color: $white;
+        padding: 3rem 0;
+    }
+
+    a 
+    {
+        text-decoration: none;
+        color: inherit;
+    }
+
+    img 
+    {
+        height: 60px;
+    }
     
 
 </style>
