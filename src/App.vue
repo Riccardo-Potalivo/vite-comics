622b03d80e631d9b09d92dc4927b5bc00f4ed104
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-11-13 15:29:30 +0100

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    components: {
+    },
+
+    data() {
+      return {
+        
+      }
+    },
+
+    methods: {
+
+    },
+
+    created() {
+
+    },
+
+    computed: {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
